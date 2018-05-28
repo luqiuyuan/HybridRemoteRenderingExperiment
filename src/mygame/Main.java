5d42e70b35fba77c64749fffc4588b2b78ef2fe0
Lu Sun <sunlufinal@hotmail.com>
2018-05-28 19:11:04 -0400

    Upgrade to use JMonkey SDK 3.2.1

--- src/mygame/Main.java
+++ src/mygame/Main.java
@@ -6,13 +6,15 @@ import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Geometry;
-import com.jme3.scene.shape.Box;
+import com.jme3.scene.Spatial;
 
 /**
  * test
  * @author normenhansen
  */
 public class Main extends SimpleApplication {
+    
+    String[] environment_model_names = {"room.j3o"};
 
     public static void main(String[] args) {
         Main app = new Main();
@@ -21,14 +23,23 @@ public class Main extends SimpleApplication {
 
     @Override
     public void simpleInitApp() {
-        Box b = new Box(1, 1, 1);
-        Geometry geom = new Geometry("Box", b);
-
-        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        mat.setColor("Color", ColorRGBA.Blue);
-        geom.setMaterial(mat);
-
-        rootNode.attachChild(geom);
+        // Setup environment models
+        for (int i = 0; i < environment_model_names.length; i++) {
+            Spatial model = assetManager.loadModel("Models-Low/" + environment_model_names[i]);
+            // Set materials
+            Material material = new Material(assetManager, "Common/MatDefs/Light/Lighting.j3md");
+            material.setColor("Diffuse", ColorRGBA.Green);
+            model.setMaterial(material);
+            model.setLocalTranslation(-10.0f, -10.0f, 30.0f);
+            model = model.scale(60.0f);
+            // Attach environment models to the root node
+            rootNode.attachChild(model);
+        }
+        
+        // Setup the camera
+        cam.setFrustumPerspective(45.0f, this.settings.getWidth() / (float)this.settings.getHeight(), 0.1f, 100000.0f);
+        cam.setLocation(new Vector3f(0.0f, 0.0f, 0.0f));
+        cam.lookAt(new Vector3f(0.0f, 0.0f, -1.0f), new Vector3f(0.0f, 1.0f, 0.0f));
     }
 
     @Override
